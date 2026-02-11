a9c14a5cb2e942c350537ee7600c8436caa68b35
Angelica-Riego-OCreen <angelica.ocreen@stu.ocsb.ca>
2026-02-11 12:42:12 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Angelica Riego-O'Creen
+ * Created on: feb 2026
+ * This program runs Hello Angelica
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Angelica!')
